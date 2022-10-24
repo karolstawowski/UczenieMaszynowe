1c07fafeb5abb8f0f2a2b0496cbd7895ecf9b7fd
Karol Stawowski <66377130+karolstawowski@users.noreply.github.com>
2022-10-24 14:20:45 +0200

    refactor: move functions to separate file

--- zad_1.py
+++ zad_1.py
@@ -1,3 +1,5 @@
+import zad_1.utils
+
 # Zadanie a
 def print_names(names):
     for name in names:
@@ -9,21 +11,8 @@ print_names(['Ania', 'Bartek', 'Szymon', 'Adam', 'Staszek'])
 
 # Zadanie b
 
-
-def multiply_numbers(numbers):
-    multiplied_numbers = []
-    for number in numbers:
-        multiplied_numbers.append(number * 2)
-
-    return multiplied_numbers
-
-
 print('\nZadanie b1')
 print(multiply_numbers([4.2, 2.1, 6, 40, 0.51]))
-
-
-def multiply_numbers(numbers):
-    return [number * 2 for number in numbers]
 
 
 print('\nZadanie b2')
@@ -32,23 +21,10 @@ print(multiply_numbers([4.2, 2.1, 6, 40, 0.51]))
 # Zadanie c
 
 
-def even_numbers(numbers):
-    for i in numbers:
-        if (i % 2 == 0):
-            print(i)
-
-
 print('\nZadanie c')
 even_numbers(range(0, 10))
 
 # Zadanie d
-
-
-def every_second_number(numbers):
-    for i in range(len(numbers)):
-        if (i % 2 != 0):
-            print(numbers[i])
-
 
 print('\nZadanie d')
 every_second_number(range(0, 10))
---
+++ zad_1/utils.py
@@ -0,0 +1,19 @@
+def multiply_numbers(numbers):
+    multiplied_numbers = []
+    for number in numbers:
+        multiplied_numbers.append(number * 2)
+
+    return multiplied_numbers
+
+def multiply_numbers(numbers):
+    return [number * 2 for number in numbers]
+
+def even_numbers(numbers):
+    for i in numbers:
+        if i % 2 == 0:
+            print(i)
+
+def every_second_number(numbers):
+    for i in range(len(numbers)):
+        if i % 2 != 0:
+            print(numbers[i])
