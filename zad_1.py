c6034213cb509c7689a5a32e0ff6ac251d0b6692
Karol Stawowski <66377130+karolstawowski@users.noreply.github.com>
2022-10-25 17:05:52 +0200

    fix: add missing files

---
+++ zad_1.py
@@ -0,0 +1,7 @@
+from zad1.utils import convert_image_to_string
+
+print(convert_image_to_string('./assets/05221859.png'))
+print(convert_image_to_string('./assets/hello_world.png'))
+print(convert_image_to_string('./assets/joy_of_data.png'))
+print(convert_image_to_string('./assets/noisy.png'))
+print(convert_image_to_string('./assets/quick_start.png'))
