c184d82d340acad0e14b34da4ea710da5ffaad75
Karol Stawowski <66377130+karolstawowski@users.noreply.github.com>
2022-10-25 17:14:49 +0200

    refactor: fix imports

--- zad_1.py
+++ zad_1.py
@@ -1,25 +1,25 @@
-import zad_1.utils
+import zad1.utils as u
 
 # Zadanie a
 
 print('\nZadanie a')
-print_names(['Ania', 'Bartek', 'Szymon', 'Adam', 'Staszek'])
+u.print_names(['Ania', 'Bartek', 'Szymon', 'Adam', 'Staszek'])
 
 # Zadanie b
 
 print('\nZadanie b1')
-print(multiply_numbers([4.2, 2.1, 6, 40, 0.51]))
+print(u.multiply_numbers([4.2, 2.1, 6, 40, 0.51]))
 
 print('\nZadanie b2')
-print(multiply_numbers([4.2, 2.1, 6, 40, 0.51]))
+print(u.multiply_numbers([4.2, 2.1, 6, 40, 0.51]))
 
 # Zadanie c
 
 
 print('\nZadanie c')
-even_numbers(range(0, 10))
+u.even_numbers(range(0, 10))
 
 # Zadanie d
 
 print('\nZadanie d')
-every_second_number(range(0, 10))
+u.every_second_number(range(0, 10))
