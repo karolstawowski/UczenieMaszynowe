0f38e6cfac893a7d8ea79278864038639c0915a8
Karol Stawowski <stawowski.dev@gmail.com>
2022-11-20 12:03:58 +0100

    feat: add image preprocessing feature

---
+++ main.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2
+
+capture = cv2.VideoCapture('input_video.MP4')
+video_output = cv2.VideoWriter('output_video.mp4',
+                               cv2.VideoWriter_fourcc(*'mp4v'),
+                               30,
+                               (1920, 1080))
+
+while True:
+    status, frame = capture.read(0)
+    if status:
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        lower_range = np.array([0, 190, 20])
+        upper_range = np.array([160, 255, 255])
+        mask = cv2.inRange(image, lower_range, upper_range)
+        result = cv2.bitwise_and(frame, frame, mask=mask)
+        result[0:370, 0:1920] = [0, 0, 0]
+        video_output.write(result)
+    else:
+        break
+
+capture.release()
+video_output.release()
+print("File saved")
