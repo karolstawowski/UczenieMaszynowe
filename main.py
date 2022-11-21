ed4e64db66457af75a134cb14c1544929930883c
Karol Stawowski <stawowski.dev@gmail.com>
2022-11-21 20:27:54 +0100

    feat: separate hsv color ranges

--- main.py
+++ main.py
@@ -7,14 +7,21 @@ video_output = cv2.VideoWriter('./assets/output_video.mp4',
                                30,
                                (1920, 1080))
 
+lower_range_lower_red = np.array([0, 100, 50])
+upper_range_lower_red = np.array([10, 255, 255])
+lower_range_upper_red = np.array([160, 100, 20])
+upper_range_upper_red = np.array([179, 255, 255])
+lower_range_b = np.array([100, 150, 0])
+upper_range_b = np.array([140, 255, 255])
+
 while True:
     status, frame = capture.read(0)
     if status:
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        lower_range = np.array([0, 190, 20])
-        upper_range = np.array([160, 255, 255])
-        mask = cv2.inRange(image, lower_range, upper_range)
-        result = cv2.bitwise_and(frame, frame, mask=mask)
+        lower_red_mask = cv2.inRange(image, lower_range_lower_red, upper_range_lower_red)
+        upper_red_mask = cv2.inRange(image, lower_range_upper_red, upper_range_upper_red)
+        blue_mask = cv2.inRange(image, lower_range_b, upper_range_b)
+        result = cv2.bitwise_and(frame, frame, mask=lower_red_mask + upper_red_mask + blue_mask)
         result[0:370, 0:1920] = [0, 0, 0]
         video_output.write(result)
     else:
